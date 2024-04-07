d930655257e1c66c9be191cb742306377a733b7a
LiSiRui <2763590149@qq.com>
2024-04-07 23:57:54 +0800

    commit

---
+++ src/leetcode/editor/cn/翻转二叉树 .java
@@ -0,0 +1,56 @@
+package leetcode.editor.cn;
+
+import javax.swing.tree.TreeNode;
+
+/**
+  * 题目Id：226; 
+  * 题目：翻转二叉树，invert-binary-tree; 
+  * 日期：2024-04-07 23:56:50
+*/
+
+class 翻转二叉树 {
+    public static void main(String[] args) {
+        Solution solution = new 翻转二叉树().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * 前后序遍历都可以
+     * 中序不行，因为先左孩子交换孩子，再根交换孩子（做完后，右孩子已经变成了原来的左孩子），再右孩子交换孩子（此时其实是对原来的左孩子做交换）
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        swapChildren(root);
+        return root;
+    }
+
+    private void swapChildren(TreeNode root) {
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
