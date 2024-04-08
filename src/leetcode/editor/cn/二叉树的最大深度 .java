dd1423ba0c547addf8d7d0e448cf360ec9ce5542
LiSiRui <2763590149@qq.com>
2024-04-08 23:52:57 +0800

    commit

---
+++ src/leetcode/editor/cn/二叉树的最大深度 .java
@@ -0,0 +1,46 @@
+package leetcode.editor.cn;
+
+/**
+  * 题目Id：104; 
+  * 题目：二叉树的最大深度，maximum-depth-of-binary-tree; 
+  * 日期：2024-04-08 23:52:08
+*/
+
+class 二叉树的最大深度 {
+    public static void main(String[] args) {
+        Solution solution = new 二叉树的最大深度().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * 递归法
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
